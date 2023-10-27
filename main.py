324a1934af12948c3b7173ceb6471db0b6edca16
dessher <github@mcjkula.com>
2023-10-28 00:30:11 +0200

    Adjusted code

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ CAPTURE_SIZE_OTHERS = (640, 480)
 
 
 def parse_arg():
-    parser = argparse.ArgumentParser(description='Test OpenNI2 and NiTE2.')
-    parser.add_argument('-w', '--window_width', type=int, default=1024,
-                        help='Specify the window width.')
+    parser = argparse.ArgumentParser(description="Test OpenNI2 and NiTE2.")
+    parser.add_argument("-w", "--window_width", type=int, default=1024, help="Specify the window width.")
     return parser.parse_args()
 
 
@@ -32,28 +31,30 @@ def draw_limb(img, ut, j1, j2, col):
 
 
 def draw_skeleton(img, ut, user, col):
-    for idx1, idx2 in [(nite2.JointType.NITE_JOINT_HEAD, nite2.JointType.NITE_JOINT_NECK),
-                       # upper body
-                       (nite2.JointType.NITE_JOINT_NECK, nite2.JointType.NITE_JOINT_LEFT_SHOULDER),
-                       (nite2.JointType.NITE_JOINT_LEFT_SHOULDER, nite2.JointType.NITE_JOINT_TORSO),
-                       (nite2.JointType.NITE_JOINT_TORSO, nite2.JointType.NITE_JOINT_RIGHT_SHOULDER),
-                       (nite2.JointType.NITE_JOINT_RIGHT_SHOULDER, nite2.JointType.NITE_JOINT_NECK),
-                       # left hand
-                       (nite2.JointType.NITE_JOINT_LEFT_HAND, nite2.JointType.NITE_JOINT_LEFT_ELBOW),
-                       (nite2.JointType.NITE_JOINT_LEFT_ELBOW, nite2.JointType.NITE_JOINT_LEFT_SHOULDER),
-                       # right hand
-                       (nite2.JointType.NITE_JOINT_RIGHT_HAND, nite2.JointType.NITE_JOINT_RIGHT_ELBOW),
-                       (nite2.JointType.NITE_JOINT_RIGHT_ELBOW, nite2.JointType.NITE_JOINT_RIGHT_SHOULDER),
-                       # lower body
-                       (nite2.JointType.NITE_JOINT_TORSO, nite2.JointType.NITE_JOINT_LEFT_HIP),
-                       (nite2.JointType.NITE_JOINT_LEFT_HIP, nite2.JointType.NITE_JOINT_RIGHT_HIP),
-                       (nite2.JointType.NITE_JOINT_RIGHT_HIP, nite2.JointType.NITE_JOINT_TORSO),
-                       # left leg
-                       (nite2.JointType.NITE_JOINT_LEFT_FOOT, nite2.JointType.NITE_JOINT_LEFT_KNEE),
-                       (nite2.JointType.NITE_JOINT_LEFT_KNEE, nite2.JointType.NITE_JOINT_LEFT_HIP),
-                       # right leg
-                       (nite2.JointType.NITE_JOINT_RIGHT_FOOT, nite2.JointType.NITE_JOINT_RIGHT_KNEE),
-                       (nite2.JointType.NITE_JOINT_RIGHT_KNEE, nite2.JointType.NITE_JOINT_RIGHT_HIP)]:
+    for idx1, idx2 in [
+        (nite2.JointType.NITE_JOINT_HEAD, nite2.JointType.NITE_JOINT_NECK),
+        # Upper Body
+        (nite2.JointType.NITE_JOINT_NECK, nite2.JointType.NITE_JOINT_LEFT_SHOULDER),
+        (nite2.JointType.NITE_JOINT_LEFT_SHOULDER, nite2.JointType.NITE_JOINT_TORSO),
+        (nite2.JointType.NITE_JOINT_TORSO, nite2.JointType.NITE_JOINT_RIGHT_SHOULDER),
+        (nite2.JointType.NITE_JOINT_RIGHT_SHOULDER, nite2.JointType.NITE_JOINT_NECK),
+        # Left Hand
+        (nite2.JointType.NITE_JOINT_LEFT_HAND, nite2.JointType.NITE_JOINT_LEFT_ELBOW),
+        (nite2.JointType.NITE_JOINT_LEFT_ELBOW, nite2.JointType.NITE_JOINT_LEFT_SHOULDER),
+        # Right Hand
+        (nite2.JointType.NITE_JOINT_RIGHT_HAND, nite2.JointType.NITE_JOINT_RIGHT_ELBOW),
+        (nite2.JointType.NITE_JOINT_RIGHT_ELBOW, nite2.JointType.NITE_JOINT_RIGHT_SHOULDER),
+        # Lower Body
+        (nite2.JointType.NITE_JOINT_TORSO, nite2.JointType.NITE_JOINT_LEFT_HIP),
+        (nite2.JointType.NITE_JOINT_LEFT_HIP, nite2.JointType.NITE_JOINT_RIGHT_HIP),
+        (nite2.JointType.NITE_JOINT_RIGHT_HIP, nite2.JointType.NITE_JOINT_TORSO),
+        # Left Leg
+        (nite2.JointType.NITE_JOINT_LEFT_FOOT, nite2.JointType.NITE_JOINT_LEFT_KNEE),
+        (nite2.JointType.NITE_JOINT_LEFT_KNEE, nite2.JointType.NITE_JOINT_LEFT_HIP),
+        # Right Leg
+        (nite2.JointType.NITE_JOINT_RIGHT_FOOT, nite2.JointType.NITE_JOINT_RIGHT_KNEE),
+        (nite2.JointType.NITE_JOINT_RIGHT_KNEE, nite2.JointType.NITE_JOINT_RIGHT_HIP),
+    ]:
         draw_limb(img, ut, user.skeleton.joints[idx1], user.skeleton.joints[idx2], col)
 
 
@@ -75,19 +76,21 @@ def capture_skeleton():
     depth_stream = dev.create_depth_stream()
     depth_stream.start()
 
-    dev_name = dev.get_device_info().name.decode('UTF-8')
-    print("device name: {}".format(dev_name))
+    dev_name = dev.get_device_info().name.decode("UTF-8")
+    print("Device Name: {}".format(dev_name))
     use_kinect = False
-    if dev_name == 'Kinect':
+    if dev_name == "Kinect":
         use_kinect = True
-        print('using Kinect.')
+        print("Using Kinect.")
 
     try:
         user_tracker = nite2.UserTracker(dev)
     except utils.NiteError:
-        print("Unable to start the NiTE human tracker. Check "
-              "the error messages in the console. Model data "
-              "(s.dat, h.dat...) might be inaccessible.")
+        print(
+            "Unable to start the NiTE human tracker. Check "
+            "the error messages in the console. Model data "
+            "(s.dat, h.dat...) might be inaccessible."
+        )
         sys.exit(-1)
 
     (img_w, img_h) = CAPTURE_SIZE_KINECT if use_kinect else CAPTURE_SIZE_OTHERS
@@ -99,8 +102,9 @@ def capture_skeleton():
 
         depth_frame = depth_stream.read_frame()
         depth_frame_data = depth_frame.get_buffer_as_uint16()
-        img = np.ndarray((depth_frame.height, depth_frame.width), dtype=np.uint16,
-                         buffer=depth_frame_data).astype(np.float32)
+        img = np.ndarray((depth_frame.height, depth_frame.width), dtype=np.uint16, buffer=depth_frame_data).astype(
+            np.float32
+        )
         if use_kinect:
             img = img[0:img_h, 0:img_w]
 
@@ -112,10 +116,12 @@ def capture_skeleton():
         if ut_frame.users:
             for user in ut_frame.users:
                 if user.is_new():
-                    print("new person id:{} detected.".format(user.id))
+                    print("New person id:{} detected.".format(user.id))
                     user_tracker.start_skeleton_tracking(user.id)
-                elif (user.state == nite2.UserState.NITE_USER_STATE_VISIBLE and
-                      user.skeleton.state == nite2.SkeletonState.NITE_SKELETON_TRACKED):
+                elif (
+                    user.state == nite2.UserState.NITE_USER_STATE_VISIBLE
+                    and user.skeleton.state == nite2.SkeletonState.NITE_SKELETON_TRACKED
+                ):
                     draw_skeleton(img, user_tracker, user, (255, 255, 255))
 
         cv2.imshow("Depth", cv2.resize(img, (win_w, win_h)))
@@ -125,5 +131,5 @@ def capture_skeleton():
     close_capture_device()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     capture_skeleton()
